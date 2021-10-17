a1d56cd59327933ee0cbc5ba6f9c61f696eb09c2
Izuco <github@izuco.dev>
2021-10-17 11:43:01 +0200

    Fixed #123

--- modules/module.getdata.js
+++ modules/module.getdata.js
@@ -23,7 +23,6 @@ const getData = async (options) => {
         url: options.url, 
         headers: {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:70.0) Gecko/20100101 Firefox/70.0',
-            'devicetype': 'Android Phone',
             'Accept-Encoding': 'gzip',
             ...regionHeaders
         }
