d66d86fb7c07f6f67dddeb00baf31a1e87f42a46
AnidlSupport <a165487221456@gmail.com>
2023-01-21 12:53:02 +0100

    Update dev.js

--- dev.js
+++ dev.js
@@ -4,8 +4,8 @@ const toRun = process.argv.slice(2).join(" ").split('---');
 
 const waitForProcess = async (proc) => {
   return new Promise((resolve, reject) => {
-    proc.stdout?.on('data', console.log);
-    proc.stderr?.on('data', console.error);
+    proc.stdout?.on('data', (data) => process.stdout.write(data));
+    proc.stderr?.on('data', (data) => process.stderr.write(data));
     proc.on('close', resolve);
     proc.on('error', reject);
   });
@@ -14,7 +14,7 @@ const waitForProcess = async (proc) => {
 (async () => {
   await waitForProcess(exec('npm run tsc test false'));
   for (let command of toRun) {
-    await waitForProcess(exec(`node index.js ${command}`, {
+    await waitForProcess(exec(`node index.js --service crunchy ${command}`, {
       cwd: path.join(__dirname, 'lib')
     }))
   }
