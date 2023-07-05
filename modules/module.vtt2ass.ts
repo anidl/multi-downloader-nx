e12d9a30f644258d4f21eaeda6f8faf1234b9ff9
AnimeDL <multidownloadernx@gmail.com>
2023-07-05 15:05:20 -0700

    Revert "eslint fixes"
    This reverts commit a17677c3bf5ad9f8b2ef31b95465e504da03c908.

--- modules/module.vtt2ass.ts
+++ modules/module.vtt2ass.ts
@@ -82,8 +82,6 @@ function parseStyle(stylegroup: string, line: string, style: any) {
   for (const s of line.split(';')) {
     if (s == '') continue;
     const st = s.trim().split(':');
-    const transformed_str = st[1].split(',').map(r => r.trim());
-    let arr = transformed_str[1].split(',').map(r => r.trim());
     let cl;
     switch (st[0]) {
     case 'font-family':
@@ -140,7 +138,8 @@ function parseStyle(stylegroup: string, line: string, style: any) {
       if (stylegroup.startsWith('Subtitle') || stylegroup.startsWith('Song')) { //don't touch shadow if dialog
         break;
       }
-      
+      let transformed_str = st[1].split(',').map(r => r.trim());
+      let arr = transformed_str[1].split(',').map(r => r.trim());
       arr = arr.map(r => { return (r.split(' ').length > 3 ? r.replace(/(\d+)px black$/, '') : r.replace(/black$/, '')).trim(); });
       transformed_str[1] = arr.map(r => r.replace(/-/g, '').replace(/px/g, '').replace(/(^| )0( |$)/g, ' ').trim()).join(' ');
       arr = transformed_str[1].split(' ');
