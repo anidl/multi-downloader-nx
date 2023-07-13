a25434b0591edbb0a3721ce66f9218bfb3f019d9
AnimeDL <multidownloadernx@gmail.com>
2023-07-13 15:14:35 -0700

    Add version statement on GUI boot

--- gui/server/index.ts
+++ gui/server/index.ts
@@ -6,6 +6,7 @@ import open from 'open';
 import path from 'path';
 import { PublicWebSocket } from './websocket';
 import { console } from '../../modules/log';
+import packageJson from '../../package.json';
 
 process.title = 'AniDL';
 
@@ -20,6 +21,8 @@ export { app, cfg };
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(workingDir, 'gui', 'server', 'build'), { maxAge: 1000 * 60 * 20 }));
+
+console.info(`\n=== Multi Downloader NX GUI ${packageJson.version} ===\n`);
 
 const server = app.listen(cfg.gui.port, () => {
   console.info(`GUI server started on port ${cfg.gui.port}`);
