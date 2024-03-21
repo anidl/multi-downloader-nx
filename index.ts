0e674fcd67e155a1fbfe98875abd63215674b2ac
AnimeDL <multidownloadernx@gmail.com>
2024-03-21 13:53:31 -0700

    Improve readability of index

--- index.ts
+++ index.ts
@@ -55,11 +55,39 @@ import update from './modules/module.updater';
         if (key.endsWith('crunchy.js') || key.endsWith('funi.js') || key.endsWith('hidive.js'))
           delete require.cache[key];
       });
-      const service = new (argv.service === 'funi' ? (await import('./funi')).default : argv.service === 'hidive' ? (await import('./hidive')).default : (await import('./crunchy')).default)(argv.debug) as ServiceClass;
+      let service: ServiceClass;
+      switch(argv.service) {
+      case 'funi':
+        service = new (await import('./funi')).default;
+        break;
+      case 'crunchy':
+        service = new (await import('./crunchy')).default;
+        break;
+      case 'hidive':
+        service = new (await import('./hidive')).default;
+        break;
+      default: 
+        service = new (await import(`./${argv.service}`)).default;
+        break;
+      }
       await service.cli();
     }
   } else {
-    const service = new (argv.service === 'funi' ? (await import('./funi')).default : argv.service === 'hidive' ? (await import('./hidive')).default : (await import('./crunchy')).default)(argv.debug) as ServiceClass;
+    let service: ServiceClass;
+    switch(argv.service) {
+    case 'funi':
+      service = new (await import('./funi')).default;
+      break;
+    case 'crunchy':
+      service = new (await import('./crunchy')).default;
+      break;
+    case 'hidive':
+      service = new (await import('./hidive')).default;
+      break;
+    default: 
+      service = new (await import(`./${argv.service}`)).default;
+      break;
+    }
     await service.cli();
   }
 })();
