9902b16c816de96a317d9ea954d770838175dd84
AnimeDL <multidownloadernx@gmail.com>
2025-01-29 09:37:49 -0800

    [CR] Switch back to android endpoints

--- modules/module.api-urls.ts
+++ modules/module.api-urls.ts
@@ -27,6 +27,7 @@ export type APIType = {
   defaultUserAgent: string,
   authBasic: string
   authBasicMob: string
+  authBasicIOS: string
   authBasicSwitch: string,
   beta_profile: string
   beta_cmsToken: string
@@ -74,18 +75,19 @@ const api: APIType = {
   session:           `${domain.api}/start_session.0.json`,
   collections:       `${domain.api}/list_collections.0.json`,
   // new api
-  beta_auth:         `${domain.www}/auth/v1/token`,
+  beta_auth:         `${domain.api_beta}/auth/v1/token`,
   // This User-Agent bypasses Cloudflare security by the newer Endpoint
   defaultUserAgent:  'Crunchyroll/4.68.2 (bundle_identifier:com.crunchyroll.iphone; build_number:4007128.533694055) iOS/18.2.0 Gravity/4.68.2',
   authBasic:         'Basic bm9haWhkZXZtXzZpeWcwYThsMHE6',
-  authBasicMob:      'Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=',
+  authBasicMob:      'Basic ZG1yeWZlc2NkYm90dWJldW56NXo6NU45aThPV2cyVmtNcm1oekNfNUNXekRLOG55SXo0QU0=',
+  authBasicIOS:      'Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=',
   authBasicSwitch:   'Basic dC1rZGdwMmg4YzNqdWI4Zm4wZnE6eWZMRGZNZnJZdktYaDRKWFMxTEVJMmNDcXUxdjVXYW4=',
-  beta_profile:      `${domain.www}/accounts/v1/me/profile`,
-  beta_cmsToken:     `${domain.www}/index/v2`,
-  search:            `${domain.www}/content/v2/discover/search`,
-  cms:               `${domain.www}/content/v2/cms`,
-  beta_browse:       `${domain.www}/content/v1/browse`,
-  beta_cms:          `${domain.www}/cms/v2`,
+  beta_profile:      `${domain.api_beta}/accounts/v1/me/profile`,
+  beta_cmsToken:     `${domain.api_beta}/index/v2`,
+  search:            `${domain.api_beta}/content/v2/discover/search`,
+  cms:               `${domain.api_beta}/content/v2/cms`,
+  beta_browse:       `${domain.api_beta}/content/v1/browse`,
+  beta_cms:          `${domain.api_beta}/cms/v2`,
   // beta api
   drm:               `${domain.api_beta}/drm/v1/auth`,
   crunchyAuthHeader: {},
